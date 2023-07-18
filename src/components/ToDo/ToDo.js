cda094d83d07c0f2b4ee4d9b91e37c8deaf32561
SerhiiRodin <rodinserj@ukr.net>
2023-07-18 20:03:11 +0300

    correct mistakes

--- src/components/ToDo/ToDo.js
+++ src/components/ToDo/ToDo.js
@@ -1,7 +1,6 @@
 import React from "react";
-import { useDispatch } from "react-redux";
+
 import { Link, useLocation } from "react-router-dom";
-import { updateCompleted } from "../../redux/todo/actions";
 
 const ToDo = ({ todo, handleCheck, handleDelete }) => {
   const location = useLocation();
